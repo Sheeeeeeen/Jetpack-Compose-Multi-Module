b42d938d97534bb3212420e945dc5423415e9cf1
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-11-14 15:33:34 +0800

    add ui test for login screen

---
+++ feature/login/src/androidTest/java/ph/com/sheen/login/LoginScreenTest.kt
@@ -0,0 +1,25 @@
+package ph.com.sheen.login
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import org.junit.Rule
+import org.junit.Test
+
+class LoginScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun check_login_screen() {
+        composeTestRule.setContent {
+            LoginScreen()
+        }
+
+        composeTestRule.onNodeWithText("Welcome!").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Teacher's App").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Login").assertIsDisplayed()
+    }
+}
