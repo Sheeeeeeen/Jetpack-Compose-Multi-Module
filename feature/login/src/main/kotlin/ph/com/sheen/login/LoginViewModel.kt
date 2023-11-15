a7073523caf67400e5427b7a1c7e4b0ae7c0a499
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-11-15 16:07:42 +0800

    add login viewmodel test

--- feature/login/src/main/kotlin/ph/com/sheen/login/LoginViewModel.kt
+++ feature/login/src/main/kotlin/ph/com/sheen/login/LoginViewModel.kt
@@ -2,8 +2,27 @@ package ph.com.sheen.login
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor() : ViewModel() {
+
+    private val _uiState: MutableStateFlow<LoginUIState> = MutableStateFlow(LoginUIState())
+    val uiState: StateFlow<LoginUIState>
+        get() = _uiState
+
+    fun setUiStateToLoading() {
+        _uiState.update {
+            it.copy(isLoading = true)
+        }
+    }
+}
+
+data class LoginUIState(
+    val isLoading: Boolean = false,
+) {
+
 }
---
+++ feature/login/src/test/kotlin/ph/com/sheen/login/LoginViewModelTest.kt
@@ -0,0 +1,47 @@
+package ph.com.sheen.login
+
+import app.cash.turbine.test
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class LoginViewModelTest {
+
+    private val testCoroutineDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+    private val testCoroutineScope = TestScope(testCoroutineDispatcher + Job())
+
+    private lateinit var viewModel: LoginViewModel
+
+    @Before
+    fun setup() {
+        viewModel = LoginViewModel()
+    }
+
+    @Test
+    fun `test Login ui state isLoading default value is false`() {
+        testCoroutineScope.runTest {
+            viewModel.uiState.test {
+                assertFalse(awaitItem().isLoading)
+            }
+        }
+    }
+
+    @Test
+    fun `test Login ui state isLoading value is true when loading is on progress`() {
+        testCoroutineScope.runTest {
+            viewModel.setUiStateToLoading()
+            viewModel.uiState.test {
+                assertTrue(awaitItem().isLoading)
+            }
+        }
+    }
+}
