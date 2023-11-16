e969ac0031fd8dbb21013b3d8af402a8659f0100
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-11-16 14:32:31 +0800

    add dependencies

--- feature/dashboard/build.gradle.kts
+++ feature/dashboard/build.gradle.kts
@@ -2,6 +2,8 @@
 plugins {
     alias(libs.plugins.com.android.library)
     alias(libs.plugins.org.jetbrains.kotlin.android)
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -35,10 +37,32 @@ android {
 
 dependencies {
 
+    implementation(project(mapOf("path" to ":core:designsystem")))
+
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material3)
+    implementation(libs.activity.compose)
+    implementation(libs.bundles.navigation)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
+
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test.jvm)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.ui.test.junit4)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+    testImplementation(libs.robolectric)
+    testImplementation(libs.turbine)
 }
