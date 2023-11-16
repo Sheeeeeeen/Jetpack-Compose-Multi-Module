c9a91c99684acd311df699f2652fa14825f0bfd1
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-11-16 15:21:04 +0800

    code enhancement

--- app/src/main/kotlin/ph/com/sheen/jetpackcomposemultimodule/MainActivity.kt
+++ app/src/main/kotlin/ph/com/sheen/jetpackcomposemultimodule/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.animation.AnticipateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.animation.doOnEnd
@@ -16,27 +17,10 @@ import ph.com.sheen.designsystem.theme.ui.JetpackComposeMultiModuleTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            splashScreen.setOnExitAnimationListener { splashScreenView ->
-                // Create your custom animation.
-                val slideUp = ObjectAnimator.ofFloat(
-                    splashScreenView,
-                    View.TRANSLATION_Y,
-                    0f,
-                    -splashScreenView.height.toFloat()
-                )
-                slideUp.interpolator = AnticipateInterpolator()
-                slideUp.duration = 2000L
-
-                // Call SplashScreenView.remove at the end of your custom animation.
-                slideUp.doOnEnd { splashScreenView.remove() }
-
-                // Run your animation.
-                slideUp.start()
-            }
-        }
+        splashScreen.addSplashScreenEffect()
         setContent {
             JetpackComposeMultiModuleTheme {
                 val navHostController = rememberNavController()
---
+++ app/src/main/kotlin/ph/com/sheen/jetpackcomposemultimodule/Utils.kt
@@ -0,0 +1,30 @@
+package ph.com.sheen.jetpackcomposemultimodule
+
+import android.animation.ObjectAnimator
+import android.os.Build
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import android.window.SplashScreen
+import androidx.core.animation.doOnEnd
+
+fun SplashScreen.addSplashScreenEffect() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        this.setOnExitAnimationListener { splashScreenView ->
+            // Create your custom animation.
+            val slideUp = ObjectAnimator.ofFloat(
+                splashScreenView,
+                View.TRANSLATION_Y,
+                0f,
+                -splashScreenView.height.toFloat()
+            )
+            slideUp.interpolator = AnticipateInterpolator()
+            slideUp.duration = 2000L
+
+            // Call SplashScreenView.remove at the end of your custom animation.
+            slideUp.doOnEnd { splashScreenView.remove() }
+
+            // Run your animation.
+            slideUp.start()
+        }
+    }
+}
