1f6f01fbef6dd358a7e00db5158b6f306295e80c
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-11-16 14:23:19 +0800

    add unit test when user is successfully login

--- feature/login/src/androidTest/kotlin/ph/com/sheen/login/LoginScreenTest.kt
+++ feature/login/src/androidTest/kotlin/ph/com/sheen/login/LoginScreenTest.kt
@@ -1,10 +1,13 @@
 package ph.com.sheen.login
 
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import org.junit.Rule
 import org.junit.Test
+import ph.com.sheen.login.model.LoginStatus
+import ph.com.sheen.login.model.LoginUIState
 
 class LoginScreenTest {
 
@@ -30,5 +33,21 @@ class LoginScreenTest {
         composeTestRule.onNodeWithTag("teachers_label").assertIsDisplayed()
         composeTestRule.onNodeWithTag("login_button").assertIsDisplayed()
         composeTestRule.onNodeWithTag("login_loading").assertIsDisplayed()
+    }
+
+    @Test
+    fun test_login_screen_when_user_login_is_successful() {
+        composeTestRule.setContent {
+            LoginScreen(
+                uiState = LoginUIState(
+                    isLoading = false,
+                    loginStatus = LoginStatus.Successful
+                ), onLoginClick = {}
+            )
+        }
+        composeTestRule.onNodeWithTag("welcome_label").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("teachers_label").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("login_button").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("login_loading").assertDoesNotExist()
     }
 }
