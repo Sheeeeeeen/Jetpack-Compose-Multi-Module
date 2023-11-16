141e4b6aa043b5b612e274d75c3a44dfd22967ab
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-11-16 15:46:50 +0800

    fixed crashing on lower env for splash screen and add code to instantiate viewmodel

--- app/src/main/kotlin/ph/com/sheen/jetpackcomposemultimodule/MainActivity.kt
+++ app/src/main/kotlin/ph/com/sheen/jetpackcomposemultimodule/MainActivity.kt
@@ -1,29 +1,29 @@
 package ph.com.sheen.jetpackcomposemultimodule
 
-import android.animation.ObjectAnimator
 import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.animation.AnticipateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.animation.doOnEnd
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ph.com.sheen.designsystem.theme.ui.JetpackComposeMultiModuleTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        splashScreen.addSplashScreenEffect()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen.addSplashScreenEffect()
+        }
+
         setContent {
             JetpackComposeMultiModuleTheme {
                 val navHostController = rememberNavController()
+                val viewModel: MainViewModel by viewModels()
                 AppNavHost(navController = navHostController)
             }
         }
