e3884dcfcdd30260e1881012abfaeb4869ed003b
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-11-16 15:18:21 +0800

    add splash screen api

--- app/src/main/java/ph/com/sheen/jetpackcomposemultimodule/MainActivity.kt
+++ app/src/main/java/ph/com/sheen/jetpackcomposemultimodule/MainActivity.kt
@@ -1,12 +1,15 @@
 package ph.com.sheen.jetpackcomposemultimodule
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.animation.doOnEnd
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ph.com.sheen.designsystem.theme.ui.JetpackComposeMultiModuleTheme
@@ -15,6 +18,25 @@ import ph.com.sheen.designsystem.theme.ui.JetpackComposeMultiModuleTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                // Create your custom animation.
+                val slideUp = ObjectAnimator.ofFloat(
+                    splashScreenView,
+                    View.TRANSLATION_Y,
+                    0f,
+                    -splashScreenView.height.toFloat()
+                )
+                slideUp.interpolator = AnticipateInterpolator()
+                slideUp.duration = 2000L
+
+                // Call SplashScreenView.remove at the end of your custom animation.
+                slideUp.doOnEnd { splashScreenView.remove() }
+
+                // Run your animation.
+                slideUp.start()
+            }
+        }
         setContent {
             JetpackComposeMultiModuleTheme {
                 val navHostController = rememberNavController()
