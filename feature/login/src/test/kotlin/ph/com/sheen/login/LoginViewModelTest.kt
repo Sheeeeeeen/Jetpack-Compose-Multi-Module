a6e250864d8400220420dba55d6b986196e8f665
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-11-18 09:46:15 +0800

    add dependency on login view model to fix compile error

--- feature/login/src/test/kotlin/ph/com/sheen/login/LoginViewModelTest.kt
+++ feature/login/src/test/kotlin/ph/com/sheen/login/LoginViewModelTest.kt
@@ -1,8 +1,15 @@
 package ph.com.sheen.login
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import androidx.test.core.app.ApplicationProvider
 import app.cash.turbine.test
 import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestDispatcher
@@ -12,19 +19,29 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import ph.com.sheen.datastore.DefaultUserDataStore
 import ph.com.sheen.login.model.LoginStatus
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(RobolectricTestRunner::class)
 class LoginViewModelTest {
 
     private val testCoroutineDispatcher: TestDispatcher = UnconfinedTestDispatcher()
     private val testCoroutineScope = TestScope(testCoroutineDispatcher + Job())
 
+    private val testContext: Context = ApplicationProvider.getApplicationContext()
+
+    private val testDataStore: DataStore<Preferences> =
+        PreferenceDataStoreFactory.create(scope = testCoroutineScope,
+            produceFile = { testContext.preferencesDataStoreFile(DefaultUserDataStore.PREFERENCE_USER_SETTING) })
+
+    private lateinit var userDataStore: DefaultUserDataStore
     private lateinit var viewModel: LoginViewModel
 
     @Before
     fun setup() {
-        viewModel = LoginViewModel()
+        userDataStore = DefaultUserDataStore(dataStore = testDataStore)
+        viewModel = LoginViewModel(userDataStore = userDataStore)
     }
 
     @Test
