6227247e43b89ebfed7d0f4259eb0ec5cded7cf5
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-11-18 09:29:23 +0800

    delete unused utils class

--- app/src/main/kotlin/ph/com/sheen/jetpackcomposemultimodule/MainActivity.kt
+++ app/src/main/kotlin/ph/com/sheen/jetpackcomposemultimodule/MainActivity.kt
@@ -1,6 +1,5 @@
 package ph.com.sheen.jetpackcomposemultimodule
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
