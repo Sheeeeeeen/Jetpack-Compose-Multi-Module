b6e10f190d8c77ae7009d586ce90b173d67778fd
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-11-18 14:39:40 +0800

    rename package from java to kotlin and add dashboard viewmodel and test

---
+++ feature/dashboard/src/main/kotlin/ph/com/sheen/dashboard/DashboardViewModel.kt
@@ -0,0 +1,10 @@
+package ph.com.sheen.dashboard
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DashboardViewModel @Inject constructor(): ViewModel() {
+
+}
---
+++ feature/dashboard/src/test/kotlin/ph/com/sheen/dashboard/DashboardViewModelTest.kt
@@ -0,0 +1,23 @@
+//package ph.com.sheen.dashboard
+//
+//import org.junit.Assert.assertTrue
+//import org.junit.Before
+//import org.junit.Test
+//
+//class DashboardViewModelTest {
+//
+//    private lateinit var viewModel: DashboardViewModel
+//
+//    @Before
+//    fun setup() {
+//        viewModel = DashboardViewModel()
+//    }
+//
+//    //retrieve list of classroom
+//    @Test
+//    fun `test to retrieve list of classroom`() {
+//        viewModel.uiState.test {
+//            assertTrue(awaitItem())
+//        }
+//    }
+//}
