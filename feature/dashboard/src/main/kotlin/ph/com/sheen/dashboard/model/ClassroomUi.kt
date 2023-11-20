bd7eda0e619d61aa16c1aab4217f38a9b087ceef
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-11-21 07:44:17 +0800

    add classroom ui model

---
+++ feature/dashboard/src/main/kotlin/ph/com/sheen/dashboard/model/ClassroomUi.kt
@@ -0,0 +1,28 @@
+package ph.com.sheen.dashboard.model
+
+import ph.com.sheen.data.model.Classroom
+import java.util.UUID
+
+@JvmInline
+value class ClassroomHeader(val value: String)
+
+@JvmInline
+value class ClassroomName(val value: String)
+
+data class ClassroomUi(
+    val id: UUID,
+    val header: ClassroomHeader,
+    val classroomName: ClassroomName,
+)
+
+fun Classroom.toUiModel(): ClassroomUi {
+    return ClassroomUi(
+        id = id,
+        header = ClassroomHeader(value = id.toString().substring(1)),
+        classroomName = ClassroomName(value = id.toString())
+    )
+}
+
+fun List<Classroom>.toListOfClassroomUiModel(): List<ClassroomUi> {
+    return this.map { it.toUiModel() }
+}
