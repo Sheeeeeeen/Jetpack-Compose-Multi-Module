485e5b50bf1212196f23c46b08665fe90e9a1359
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-11-20 12:51:24 +0800

    add viewmodel inject to dashboard screen

--- feature/dashboard/src/main/kotlin/ph/com/sheen/dashboard/DashboardScreen.kt
+++ feature/dashboard/src/main/kotlin/ph/com/sheen/dashboard/DashboardScreen.kt
@@ -4,10 +4,11 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import ph.com.sheen.designsystem.theme.ui.AppPreview
 
 @Composable
-fun DashboardRoute() {
+fun DashboardRoute(viewModel: DashboardViewModel = hiltViewModel()) {
     DashboardScreen()
 }
 
--- feature/dashboard/src/main/kotlin/ph/com/sheen/dashboard/DashboardViewModel.kt
+++ feature/dashboard/src/main/kotlin/ph/com/sheen/dashboard/DashboardViewModel.kt
@@ -2,9 +2,14 @@ package ph.com.sheen.dashboard
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class DashboardViewModel @Inject constructor(): ViewModel() {
+class DashboardViewModel @Inject constructor() : ViewModel() {
+
+    val uiState = MutableStateFlow(DashboardUiState())
 
 }
+
+data class DashboardUiState(val classrooms: List<String> = emptyList())
