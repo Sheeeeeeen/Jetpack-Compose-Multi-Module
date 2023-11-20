adeea46f55eefe845e0099f56df9fcfb28820ffc
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-11-20 08:27:22 +0800

    add AndroidJUnit4 test runner dependency

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.junit.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
