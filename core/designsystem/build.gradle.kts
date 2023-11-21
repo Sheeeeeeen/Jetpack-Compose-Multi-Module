1ce81f6a30928c3f36f7a3a60dcd8024f4f8df39
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-11-21 14:26:53 +0800

    add foundation library to import anchor draggable function

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
