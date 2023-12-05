bd0c42bc1c10ae9f8b50dda08f82044e87ffa523
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-12-05 08:28:33 +0800

    add failing test_top_app_bar_with_close_button_title_and_save_button test

--- app/src/androidTest/kotlin/ph/com/sheen/dashboard/dialog/CreateClassroomComponentTest.kt
+++ app/src/androidTest/kotlin/ph/com/sheen/dashboard/dialog/CreateClassroomComponentTest.kt
@@ -1,6 +1,10 @@
 package ph.com.sheen.dashboard.dialog
 
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import org.junit.Rule
 import org.junit.Test
 
@@ -16,5 +20,15 @@ class CreateClassroomComponentTest {
                 AppBar()
             }
         }
+        //close button
+        composeTestRule.onNodeWithTag(testTag = "close_button").assertIsDisplayed()
+        composeTestRule.onNodeWithTag(testTag = "close_button").assertExists()
+        composeTestRule.onNodeWithTag(testTag = "close_button").assertIsEnabled()
+
+        //title
+        composeTestRule.onNodeWithText(text = "Create new classroom").assertIsDisplayed()
+
+        //save button
+        composeTestRule.onNodeWithTag(testTag = "save_button")
     }
 }
