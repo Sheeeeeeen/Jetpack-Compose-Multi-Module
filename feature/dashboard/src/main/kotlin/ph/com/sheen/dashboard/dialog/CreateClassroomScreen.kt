00a23af85c0537ec2cc9a8410a97afc773f26f21
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-12-12 15:45:19 +0800

    add code to handle error mesg

--- feature/dashboard/src/main/kotlin/ph/com/sheen/dashboard/dialog/CreateClassroomScreen.kt
+++ feature/dashboard/src/main/kotlin/ph/com/sheen/dashboard/dialog/CreateClassroomScreen.kt
@@ -2,6 +2,7 @@
 
 package ph.com.sheen.dashboard.dialog
 
+import android.widget.Toast
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -11,6 +12,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -30,8 +32,7 @@ fun CreateClassroomDialog(
         properties = DialogProperties(usePlatformDefaultWidth = false)
     ) {
         CreateClassroomScreen(
-            onTappedClose = onDismissRequest,
-            onTappedSave = onTappedSave
+            onTappedClose = onDismissRequest, onTappedSave = onTappedSave
         )
     }
 }
@@ -42,6 +43,9 @@ fun CreateClassroomScreen(
     onTappedClose: () -> Unit = {},
     onTappedSave: (SavedCreatedClassroom) -> Unit = {},
 ) {
+
+    val context = LocalContext.current
+
     var selectedCategory by rememberSaveable { mutableStateOf("") }
 
     var level by rememberSaveable { mutableStateOf("") }
@@ -49,6 +53,26 @@ fun CreateClassroomScreen(
     var courseName by rememberSaveable { mutableStateOf("") }
 
     val isCollegeSelected by derivedStateOf { selectedCategory == SelectionCategory.COLLEGE.value }
+
+    val isFormCompleted by derivedStateOf {
+        if (selectedCategory == SelectionCategory.COLLEGE.value) {
+            courseName.isNotBlank() && level.isNotBlank()
+        } else {
+            level.isNotBlank()
+        }
+    }
+
+    val errorMessage by derivedStateOf {
+        when {
+            selectedCategory == SelectionCategory.COLLEGE.value && courseName.isBlank() -> "Please input course name!"
+
+            selectedCategory == SelectionCategory.COLLEGE.value && level.isBlank() -> "Please input course level!"
+
+            selectedCategory != SelectionCategory.COLLEGE.value && level.isBlank() -> "Please input level!"
+
+            else -> "Unknown Error!"
+        }
+    }
 
     BuildCreateClassroomScreen {
         Container(modifier = modifier) {
@@ -63,13 +87,10 @@ fun CreateClassroomScreen(
                 })
                 FillUpSpace()
                 SaveButton(onTappedSave = {
-                    val isFormCompleted = if (selectedCategory == SelectionCategory.COLLEGE.value) {
-                        courseName.isNotBlank() && level.isNotBlank()
-                    } else {
-                        level.isNotBlank()
-                    }
-                    if (isFormCompleted.not())
+                    if (isFormCompleted.not()) {
+                        Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show()
                         return@SaveButton
+                    }
                     onTappedSave(
                         SavedCreatedClassroom(
                             category = selectedCategory,
