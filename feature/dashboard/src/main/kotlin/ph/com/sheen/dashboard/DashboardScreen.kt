4299819f9a47d6227f7f0760f73c7eb0bab8fb6b
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-12-12 10:08:09 +0800

    code enhancement to handle on configuration changes when dialog is open

--- feature/dashboard/src/main/kotlin/ph/com/sheen/dashboard/DashboardScreen.kt
+++ feature/dashboard/src/main/kotlin/ph/com/sheen/dashboard/DashboardScreen.kt
@@ -4,8 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -40,7 +40,7 @@ fun DashboardScreen(
     scope: CoroutineScope = rememberCoroutineScope(),
 ) {
 
-    var openCreateClassroomDialog by remember { mutableStateOf(false) }
+    var openCreateClassroomDialog by rememberSaveable { mutableStateOf(false) }
 
     BuildDashboardScreen {
         Container(modifier = modifier,
