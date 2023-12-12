1ef8c2743e41910622044371613b7aa66afdcbb5
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-12-12 10:58:07 +0800

    refactor creating mock classroom entity

--- data/src/test/kotlin/ph/com/sheen/data/DefaultClassroomRepositoryTest.kt
+++ data/src/test/kotlin/ph/com/sheen/data/DefaultClassroomRepositoryTest.kt
@@ -15,6 +15,7 @@ import ph.com.sheen.database.AppDatabase
 import ph.com.sheen.database.dao.ClassroomDao
 import ph.com.sheen.database.entity.ClassroomEntity
 import java.util.UUID
+import kotlin.random.Random
 
 @RunWith(RobolectricTestRunner::class)
 class DefaultClassroomRepositoryTest {
@@ -112,9 +113,21 @@ class DefaultClassroomRepositoryTest {
         Assert.assertTrue(retrieveClassroom == updatedClassroom)
     }
 
-    private fun createClassroomEntity(): ClassroomEntity {
+    private fun createClassroomEntity(
+        category: String = UUID.randomUUID().toString(),
+        level: Byte = Random.nextBytes(10)[0],
+        courseName: String = UUID.randomUUID().toString(),
+        createdDate: Long = System.currentTimeMillis(),
+        lastUpdateDate: Long = System.currentTimeMillis(),
+    ): ClassroomEntity {
         val id = UUID.randomUUID()
-        val updateDate = System.currentTimeMillis()
-        return ClassroomEntity(id = id, lastUpdateDate = updateDate)
+        return ClassroomEntity(
+            id = id,
+            category = category,
+            level = level,
+            courseName = courseName,
+            createdDate = createdDate,
+            lastUpdateDate = lastUpdateDate
+        )
     }
 }
