74f637696af08e5460b4a36d9ff6d6526276dde8
Sheen Fabile <sheen.fabile@globe.com.ph>
2023-12-13 13:50:39 +0800

    ui test refactor

--- feature/dashboard/src/androidTest/kotlin/ph/com/sheen/dashboard/dialog/CreateClassroomScreenTest.kt
+++ feature/dashboard/src/androidTest/kotlin/ph/com/sheen/dashboard/dialog/CreateClassroomScreenTest.kt
@@ -3,10 +3,12 @@
 package ph.com.sheen.dashboard.dialog
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
@@ -31,117 +33,149 @@ class CreateClassroomScreenTest {
 
     @Test
     fun test_initial_state_screen() {
-        //close button
-        composeTestRule.onNodeWithTag(testTag = "close_button").assertIsDisplayed()
+        with(composeTestRule) {
+            //close button
+            composeTestRule.onNodeWithTag(testTag = "close_button").assertIsDisplayed()
 
-        //title
-        composeTestRule.onNodeWithText(text = "Create new classroom").assertIsDisplayed()
+            //title
+            composeTestRule.onNodeWithText(text = "Create new classroom").assertIsDisplayed()
 
-        //main content
-        composeTestRule.onNodeWithTag(testTag = "category_dropdown_menu_field_tag")
-            .assertIsDisplayed()
-        composeTestRule.onNodeWithTag(testTag = "course_name_field_tag").assertIsNotDisplayed()
-        composeTestRule.onNodeWithTag(testTag = "year_level_field_tag").assertIsDisplayed()
-        composeTestRule.onNodeWithTag(testTag = "text_save_button_tag").assertIsDisplayed()
+            //main content
+            categoryDropdownField().assertIsDisplayed()
+            courseNameTextField().assertIsNotDisplayed() //show the `courseNameTextField` if the selected category is College.
+            yearLevelTextField().assertIsDisplayed()
+            saveButton().assertIsDisplayed()
+        }
     }
 
     @Test
     fun test_show_course_name_when_category_field_is_college() {
-        //tap the category field dropdown
-        composeTestRule.onNodeWithTag(testTag = "category_dropdown_menu_field_tag").performClick()
-        composeTestRule.onNodeWithText(text = "College").performClick()
 
+        with(composeTestRule) {
+            //setup
+            val selectedCategory = SelectionCategory.COLLEGE.value
 
-        //close button
-        composeTestRule.onNodeWithTag(testTag = "close_button").assertIsDisplayed()
+            //action
+            categoryDropdownField().performClick()
+            composeTestRule.onNodeWithText(text = selectedCategory).performClick()
 
-        //title
-        composeTestRule.onNodeWithText(text = "Create new classroom").assertIsDisplayed()
-
-        //main content
-        composeTestRule.onNodeWithTag(testTag = "category_dropdown_menu_field_tag")
-            .assertIsDisplayed()
-        composeTestRule.onNodeWithTag(testTag = "course_name_field_tag").assertIsDisplayed()
-        composeTestRule.onNodeWithTag(testTag = "year_level_field_tag").assertIsDisplayed()
-        composeTestRule.onNodeWithTag(testTag = "text_save_button_tag").assertIsDisplayed()
+            //assert
+            categoryDropdownField().assertIsDisplayed()
+            courseNameTextField().assertIsDisplayed()
+            yearLevelTextField().assertIsDisplayed()
+            saveButton().assertIsDisplayed()
+        }
     }
 
     @Test
     fun test_select_category_kinder_and_input_level() {
-        //setup
-        //action
-        composeTestRule.onNodeWithTag(testTag = "year_level_field_tag").performTextInput("1")
 
-        //assert
-        //main content
-        composeTestRule.onNodeWithTag(testTag = "category_field_tag").assert(hasText("Kinder"))
-        composeTestRule.onNodeWithTag(testTag = "year_level_field_tag").assert(hasText("1"))
+        with(composeTestRule) {
+            //setup
+            val typedLevel = "1"
+            val typedCategory = SelectionCategory.KINDER.value
 
-        composeTestRule.onNodeWithTag(testTag = "category_dropdown_menu_field_tag")
-            .assertIsDisplayed()
-        composeTestRule.onNodeWithTag(testTag = "year_level_field_tag").assertIsDisplayed()
-        composeTestRule.onNodeWithTag(testTag = "text_save_button_tag").assertIsDisplayed()
+            //action
+            yearLevelTextField().performTextInput(typedLevel)
+
+            //assert
+            categoryTextField().assert(hasText(typedCategory))
+            yearLevelTextField().assert(hasText(typedLevel))
+
+            categoryDropdownField().assertIsDisplayed()
+            yearLevelTextField().assertIsDisplayed()
+            saveButton().assertIsDisplayed()
+        }
     }
 
     @Test
     fun test_select_category_junior_high_and_input_level() {
-        //setup
-        //action
-        composeTestRule.onNodeWithTag(testTag = "category_dropdown_menu_field_tag").performClick()
-        composeTestRule.onNodeWithText(text = SelectionCategory.JUNIOR_HIGH.value).performClick()
-        composeTestRule.onNodeWithTag(testTag = "year_level_field_tag").performTextInput("1")
+        with(composeTestRule) {
+            //setup
+            val typedLevel = "1"
+            val selectedCategory = SelectionCategory.JUNIOR_HIGH.value
 
-        //assert
-        //main content
-        composeTestRule.onNodeWithTag(testTag = "category_field_tag")
-            .assert(hasText(SelectionCategory.JUNIOR_HIGH.value))
-        composeTestRule.onNodeWithTag(testTag = "year_level_field_tag").assert(hasText("1"))
+            //action
+            categoryDropdownField().performClick()
+            composeTestRule.onNodeWithText(text = selectedCategory).performClick()
+            yearLevelTextField().performTextInput(typedLevel)
 
-        composeTestRule.onNodeWithTag(testTag = "category_dropdown_menu_field_tag")
-            .assertIsDisplayed()
-        composeTestRule.onNodeWithTag(testTag = "year_level_field_tag").assertIsDisplayed()
-        composeTestRule.onNodeWithTag(testTag = "text_save_button_tag").assertIsDisplayed()
+            //assert
+            //main content
+            categoryTextField().assert(hasText(selectedCategory))
+            yearLevelTextField().assert(hasText(typedLevel))
+
+            categoryDropdownField().assertIsDisplayed()
+            yearLevelTextField().assertIsDisplayed()
+            saveButton().assertIsDisplayed()
+        }
     }
 
     @Test
     fun test_select_category_senior_high_and_input_level() {
-        //setup
-        //action
-        composeTestRule.onNodeWithTag(testTag = "category_dropdown_menu_field_tag").performClick()
-        composeTestRule.onNodeWithText(text = SelectionCategory.SENIOR_HIGH.value).performClick()
-        composeTestRule.onNodeWithTag(testTag = "year_level_field_tag").performTextInput("1")
+        with(composeTestRule) {
+            //setup
+            val typedLevel = "1"
+            val selectedCategory = SelectionCategory.SENIOR_HIGH.value
 
-        //assert
-        //main content
-        composeTestRule.onNodeWithTag(testTag = "category_field_tag")
-            .assert(hasText(SelectionCategory.SENIOR_HIGH.value))
-        composeTestRule.onNodeWithTag(testTag = "year_level_field_tag").assert(hasText("1"))
+            //action
+            categoryDropdownField().performClick()
+            composeTestRule.onNodeWithText(text = selectedCategory).performClick()
+            yearLevelTextField().performTextInput(typedLevel)
 
-        composeTestRule.onNodeWithTag(testTag = "category_dropdown_menu_field_tag")
-            .assertIsDisplayed()
-        composeTestRule.onNodeWithTag(testTag = "year_level_field_tag").assertIsDisplayed()
-        composeTestRule.onNodeWithTag(testTag = "text_save_button_tag").assertIsDisplayed()
+            //assert
+            categoryTextField().assert(hasText(selectedCategory))
+            yearLevelTextField().assert(hasText(typedLevel))
+
+            categoryDropdownField().assertIsDisplayed()
+            yearLevelTextField().assertIsDisplayed()
+            saveButton().assertIsDisplayed()
+        }
     }
 
     @Test
     fun test_select_category_college_high_and_input_level() {
-        //setup
-        //action
-        composeTestRule.onNodeWithTag(testTag = "category_dropdown_menu_field_tag").performClick()
-        composeTestRule.onNodeWithText(text = SelectionCategory.COLLEGE.value).performClick()
-        composeTestRule.onNodeWithTag(testTag = "year_level_field_tag").performTextInput("1")
-        composeTestRule.onNodeWithTag(testTag = "course_name_field_tag").performTextInput("Coet")
-        //assert
-        //main content
-        composeTestRule.onNodeWithTag(testTag = "category_field_tag")
-            .assert(hasText(SelectionCategory.COLLEGE.value))
-        composeTestRule.onNodeWithTag(testTag = "year_level_field_tag").assert(hasText("1"))
-        composeTestRule.onNodeWithTag(testTag = "course_name_field_tag").assert(hasText("Coet"))
-        composeTestRule.onNodeWithTag(testTag = "course_name_field_tag").assertIsDisplayed()
+        with(composeTestRule) {
+            //setup
+            val typedLevel = "1"
+            val selectedCategory = SelectionCategory.COLLEGE.value
+            val typeCourseName = "Coet"
 
-        composeTestRule.onNodeWithTag(testTag = "category_dropdown_menu_field_tag")
-            .assertIsDisplayed()
-        composeTestRule.onNodeWithTag(testTag = "year_level_field_tag").assertIsDisplayed()
-        composeTestRule.onNodeWithTag(testTag = "text_save_button_tag").assertIsDisplayed()
+            //action
+            categoryDropdownField().performClick()
+            composeTestRule.onNodeWithText(text = selectedCategory).performClick()
+            yearLevelTextField().performTextInput(typedLevel)
+            courseNameTextField().performTextInput(typeCourseName)
+
+            //assert
+            categoryTextField().assert(hasText(SelectionCategory.COLLEGE.value))
+            yearLevelTextField().assert(hasText(typedLevel))
+            courseNameTextField().assert(hasText(typeCourseName))
+            courseNameTextField().assertIsDisplayed()
+
+            categoryDropdownField().assertIsDisplayed()
+            yearLevelTextField().assertIsDisplayed()
+            saveButton().assertIsDisplayed()
+        }
+    }
+
+    private fun ComposeContentTestRule.categoryDropdownField(): SemanticsNodeInteraction {
+        return onNodeWithTag(testTag = "category_dropdown_menu_field_tag")
+    }
+
+    private fun ComposeContentTestRule.categoryTextField(): SemanticsNodeInteraction {
+        return onNodeWithTag(testTag = "category_field_tag")
+    }
+
+    private fun ComposeContentTestRule.courseNameTextField(): SemanticsNodeInteraction {
+        return onNodeWithTag(testTag = "course_name_field_tag")
+    }
+
+    private fun ComposeContentTestRule.yearLevelTextField(): SemanticsNodeInteraction {
+        return onNodeWithTag(testTag = "year_level_field_tag")
+    }
+
+    private fun ComposeContentTestRule.saveButton(): SemanticsNodeInteraction {
+        return onNodeWithTag(testTag = "text_save_button_tag")
     }
 }
